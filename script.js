602eee609fd1565e5de2560d7f74a03149bf0e19
vermakusum593 <vermakusum593@gmail.com>
2024-11-26 21:09:00 +0100

    updated script.js

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-
-const words = ["dependent",
-  "dog",
-  "superficial",
-  "admit",
-  "juice",
-  "javascript",
-  "developer",
-  "airplane",
-  "great",
-  "fun",
-  "manipulate",
-  "cat",
-  "transition",
-  "school",
-  "computer",
-  "programming",
-  "drag",
-  "loving",
-  "north",];
-
+const words = [
+  "dependent", "dog", "superficial", "admit", "juice", "javascript", "developer", 
+  "airplane", "great", "fun", "manipulate", "cat", "transition", "school", 
+  "computer", "programming", "drag", "loving", "north"
+];
 
 const wordDisplay = document.getElementById("word-display");
 const textInput = document.getElementById("text-input");
@@ -26,26 +10,22 @@ const scoreEl = document.getElementById("score");
 const timeEl = document.getElementById("time");
 const gameOverEl = document.getElementById("game-over");
 const finalScoreEl = document.getElementById("final-score");
-const restartBtn = document.getElementById("restart-btn")
+const restartBtn = document.getElementById("restart-btn");
 
 let score = 0;
 let time = 10;
 let interval;
-
-
-
+let randomWord; 
 
 function addWordToDOM() {
-  const randomWord = words[Math.floor(Math.random() * words.length)];
+  randomWord = words[Math.floor(Math.random() * words.length)]; 
   wordDisplay.textContent = randomWord;
 }
-
 
 function updateScore() {
   score++;
   scoreEl.textContent = score;
 }
-
 
 function updateTime() {
   time--;
@@ -57,13 +37,11 @@ function updateTime() {
   }
 }
 
-
 function gameOver() {
   gameOverEl.classList.remove("hidden");
   document.getElementById("game-container").classList.add("hidden");
   finalScoreEl.textContent = score;
 }
-
 
 function restartGame() {
   score = 0;
@@ -77,7 +55,6 @@ function restartGame() {
   interval = setInterval(updateTime, 1000);
 }
 
-
 textInput.addEventListener("input", () => {
   const typedWord = textInput.value.trim();
   const displayedWord = wordDisplay.textContent;
@@ -86,14 +63,12 @@ textInput.addEventListener("input", () => {
     updateScore();
     addWordToDOM();
     textInput.value = "";
-    time += 5; 
-    updateTime(); 
+    time += 5;
+    updateTime();
   }
 });
 
-
 restartBtn.addEventListener("click", restartGame);
-
 
 addWordToDOM();
 interval = setInterval(updateTime, 1000);
